3b2262ff59daa9a645d6ac75d258c93a23c15a58
UdeSA <estudiante@intraudesa.edu.ar>
2026-04-22 17:16:54 -0300

    paso 1 del tp script 1

---
+++ TP2/scripts/0.1scrapinng.R
@@ -0,0 +1,4 @@
+# Carga de librerías vistas en clase
+library(rvest)      # Para el scraping
+library(tidyverse)  # Para manejar los datos (dplyr, ggplot2, etc.)
+library(tidytext)   # Para el procesamiento de texto (NLP)
